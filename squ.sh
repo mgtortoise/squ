f947d00fa3123432e674f8dd09417d2f1e22ca4d
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-06-17 18:24:08 +0630

    Update squ.sh

--- squ.sh
+++ squ.sh
@@ -3,5 +3,5 @@ apt install squid -y
 wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/squid.conf
 rm -rf /etc/squid/squid.conf
 mv squid.conf /etc/squid/squid.conf
-systemctl restart squid
+service squid restart
 netstat -tulpn 
